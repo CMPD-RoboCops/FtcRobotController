f5b18448fbc5298afb348ae0f8e5c901b476ea8c
mbruckart <michael.bruckart@gmail.com>
2024-02-09 21:23:30 -0500

    Merge pull request #731 from FIRST-Tech-Challenge/20230929-083754-release-candidate
    FtcRobotController v9.0.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptScanServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptScanServo.java
@@ -1,92 +1,81 @@
-package org.firstinspires.ftc.teamcode;/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoControllerEx;
 
 
-/*
- * This OpMode scans a single servo back and forward until Stop is pressed.
- * The code is structured as a LinearOpMode
- * INCREMENT sets how much to increase/decrease the servo position each cycle
- * CYCLE_MS sets the update period.
- *
- * This code assumes a Servo configured with the name "left_hand" as is found on a Robot.
- *
- * NOTE: When any servo position is set, ALL attached servos are activated, so ensure that any other
- * connected servos are able to move freely before running this test.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
- */
 @TeleOp(name = "Servo Test", group = "Test")
 //@Disabled
 public class ConceptScanServo extends LinearOpMode {
 
+    //Both Bridge Servos in correct position at 0.2
+    //Port Arm Servo in correct position at 0.4 to 0.5, but start it at 0.4
+    //Starboard Arm Servo 0.86 to 0.91, start it at .91
+    //Drone Servo...0.15 is the ready position, 0.3 is launch position
+    //Arm Servos 0.9 is your scoring position
 
     static final double INCREMENT   = 0.01;     // amount to slew servo each CYCLE_MS cycle
     static final int    CYCLE_MS    =   50;     // period of each cycle
-    static final double MAX_POS     =  0.3;     // Maximum rotational position
-    static final double MIN_POS     =  0.1;     // Minimum rotational position
+    static final double MAX_POS     =  0.9;     // Maximum rotational position
+    static final double MIN_POS     =  0.8;     // Minimum rotational position
+
+    //double  position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
+    double position = 0.9;
+    boolean rampUp = true;
+
 
     // Define class members
 
-    private Servo portarmservo = null;
-    private Servo starboardarmservo = null;
+    private Servo portarmservo = null;  //Port Arm Servo
+    private Servo starboardarmservo = null; //Starboard Arm Servo
     private Servo portbridgeservo = null; //Port bridge service
     private Servo starboardbridgeservo = null; //Starboard bridge service
     private Servo droneservo = null; //Drone Servo
     private Servo portclawservo = null; //Port claw servo
     private Servo starboardclawservo = null; //Starboard claw servo
+    private DcMotor armright = null; //Arm motor right
+    private DcMotor armleft = null; //Arm motor left
 
+    //Arm Position Variables
+    double armmax = 8;
+    double armposition=0;
 
-    double  position = (MAX_POS - MIN_POS) / 2; // Start at halfway position
-    //double position = 1;
-    boolean rampUp = true;
 
 
     @Override
     public void runOpMode() {
 
+
         //Initialize Servos
-        //droneservo = hardwareMap.get(Servo.class, "drone servo");
-        //starboardarmservo = hardwareMap.get(Servo.class, "starboard arm servo");
+        droneservo = hardwareMap.get(Servo.class, "drone servo");
+        starboardarmservo = hardwareMap.get(Servo.class, "starboard arm servo");
         starboardbridgeservo = hardwareMap.get(Servo.class, "starboard bridge servo");
-        //starboardclawservo = hardwareMap.get(Servo.class, "starboard claw servo");
-        //portarmservo = hardwareMap.get(Servo.class, "port arm servo");
+        starboardclawservo = hardwareMap.get(Servo.class, "starboard claw servo");
+        portarmservo = hardwareMap.get(Servo.class, "port arm servo");
         portbridgeservo = hardwareMap.get(Servo.class, "port bridge servo");
-        //portclawservo = hardwareMap.get(Servo.class, "port claw servo");
+        portclawservo = hardwareMap.get(Servo.class, "port claw servo");
+
+        //Reversing of servos
         starboardbridgeservo.setDirection(Servo.Direction.REVERSE);
+        portarmservo.setDirection(Servo.Direction.REVERSE);
+        droneservo.setDirection(Servo.Direction.REVERSE);
+
+        //Assign Controllers
+        ServoControllerEx PortArmServoController = (ServoControllerEx) portarmservo.getController();
+        ServoControllerEx StarboardArmServoController = (ServoControllerEx) starboardarmservo.getController();
+        ServoControllerEx PortBridgeServoController = (ServoControllerEx) portarmservo.getController();
+        ServoControllerEx StarboardBridgeServoController = (ServoControllerEx) starboardarmservo.getController();
+
+        //Initialize Arm Motors
+        armright = hardwareMap.get(DcMotor.class, "arm right");
+        armleft = hardwareMap.get(DcMotor.class, "arm left");
+
+        //Reversing Motors
+        armleft.setDirection(DcMotor.Direction.REVERSE);
+        armright.setDirection(DcMotor.Direction.REVERSE);
 
 
         // Wait for the start button
@@ -94,8 +83,11 @@ public class ConceptScanServo extends LinearOpMode {
         telemetry.update();
         waitForStart();
 
+        //starboardbridgeservo.setPosition(0.2);
+        //portbridgeservo.setPosition(0.2);
 
-            // Scan servo till stop pressed.
+
+        // Scan servo till stop pressed.
             while(opModeIsActive()){
 
                 // slew the servo, according to the rampUp (direction) variable.
@@ -126,20 +118,95 @@ public class ConceptScanServo extends LinearOpMode {
                 //telemetry.update();
 
                 // Set the servo to the new position and pause;
-                starboardbridgeservo.setPosition(position);
-                portbridgeservo.setPosition(position);
 
-                telemetry.addData("Starboard Position", "%5.2f", starboardbridgeservo.getPosition());
-                telemetry.addData("Port Position     ","%5.2f",portbridgeservo.getPosition());
-                telemetry.update();
+                //starboardarmservo.setPosition(position);
+                //portarmservo.setPosition(position);
 
+                //droneservo.setPosition(position);
+
+                //telemetry.addData("Drone Position", "%5.2f", droneservo.getPosition());
+
+                //telemetry.addData("Starboard Position", "%5.2f", starboardarmservo.getPosition());
+                //telemetry.addData("Port Position     ","%5.2f",portarmservo.getPosition());
+                //telemetry.update();
+
+                double armPower = 1;
+
+                //Both Bridge Servos in correct position at 0.2
+                //Port Claw Servo in correct position at 0.4 to 0.5, but start it at 0.4
+                //Starboard Claw Servo 0.86 to 0.91, start it at .91
+                //Drone Servo...0.15 is the ready position, 0.3 is launch position
+                //Arm Servos 0.9 is your scoring position
+
+                portclawservo.setPosition(.46);
+                starboardclawservo.setPosition(.86);
+                droneservo.setPosition(.15);
+                portarmservo.setPosition(.85);
+                starboardarmservo.setPosition(.85);
+
+                if(gamepad1.x)
+                {
+                    PortArmServoController.setServoPwmEnable(portarmservo.getPortNumber());
+                    StarboardArmServoController.setServoPwmEnable(starboardarmservo.getPortNumber());
+                    PortBridgeServoController.setServoPwmEnable(portarmservo.getPortNumber());
+                    StarboardBridgeServoController.setServoPwmEnable(starboardbridgeservo.getPortNumber());
+
+                    while(armposition<armmax)
+                    {
+                        armleft.setPower(-armPower);
+                        armright.setPower(armPower);
+                        sleep(100);
+                        armleft.setPower(0);
+                        armright.setPower(0);
+                        armposition++;
+                    }
+                    armleft.setPower(-0.1);
+                    armright.setPower(0.1);
+
+                    starboardbridgeservo.setPosition(0.2);
+                    portbridgeservo.setPosition(0.2);
+                    portarmservo.setPosition(0.9);
+                    starboardarmservo.setPosition(0.9);
+                 }
+
+                if(gamepad1.a)
+                {
+                    portclawservo.setPosition(0.35);
+                    starboardclawservo.setPosition(.91);
+                }
+
+
+                //Arm Retract
+                if(gamepad1.y)
+                {
+                    portarmservo.setPosition(.85);
+                    starboardarmservo.setPosition(.85);
+                    starboardbridgeservo.setPosition(1);
+                    portbridgeservo.setPosition(1);
+
+                    while(armposition>0)
+                    {
+                        armleft.setPower(armPower);
+                        armright.setPower(-armPower);
+                        sleep(100);
+                        armleft.setPower(0);
+                        armright.setPower(0);
+                        armposition--;
+                    }
+                    armleft.setPower(0);
+                    armright.setPower(0);
+                    PortArmServoController.setServoPwmDisable(portarmservo.getPortNumber());
+                    StarboardArmServoController.setServoPwmDisable(starboardarmservo.getPortNumber());
+                    PortBridgeServoController.setServoPwmDisable(portarmservo.getPortNumber());
+                    StarboardBridgeServoController.setServoPwmDisable(starboardbridgeservo.getPortNumber());
+                }
 
                 sleep(CYCLE_MS);
             idle();
         }
 
         // Signal done;
-        //telemetry.addData(">", "Done");
-        //telemetry.update();
+        telemetry.addData(">", "Done");
+        telemetry.update();
     }
 }
